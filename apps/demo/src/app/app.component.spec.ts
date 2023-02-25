5d1d88d17c13e3b4f4d5c2039b20d2b7a96cc3ac
ivonederlof <ivo.nederlof@vonlof.com>
2023-02-25 11:11:05 +0100

    Unit test failure

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {NgxIconComponent, provideIcons} from '@vonlof/ngx-icons';
+import {logoGithubMark} from '../icons/generated';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [NgxIconComponent],
+      providers: [
+        provideIcons([logoGithubMark])
+      ]
     }).compileComponents();
   });
 

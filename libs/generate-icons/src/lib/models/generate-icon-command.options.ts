85d8921cee9a43d959ffe6d22dec7398d2567933
ivonederlof <ivo.nederlof@vonlof.com>
2023-03-06 14:14:42 +0100

    refactor(generate-icons): removed command properties in model

--- libs/generate-icons/src/lib/models/generate-icon-command.options.ts
+++ libs/generate-icons/src/lib/models/generate-icon-command.options.ts
@@ -2,10 +2,5 @@ import { LogLevel } from '../utils/logger.util';
 
 export interface GenerateIconCommandOptions {
   config: string;
-  src?: string;
-  output?: string;
-  prefix?: string;
-  suffix?: string;
-  barrel?: boolean;
   logger?: LogLevel;
 }
